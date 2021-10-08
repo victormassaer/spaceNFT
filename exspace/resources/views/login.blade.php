67c037dc1dbdd60dcd73821bee7e74b4de993ae5
TheBreasy <b.cautaerts@hotmail.be>
2021-10-08 15:53:35 +0200

    added tailwindcss in project

--- exspace/resources/views/login.blade.php
+++ exspace/resources/views/login.blade.php
@@ -5,9 +5,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>Login | eXspace - Expand your galaxy</title>
 </head>
 <body>
-<h1>Login</h1>
+    <h1>Login</h1>
 </body>
 </html>
