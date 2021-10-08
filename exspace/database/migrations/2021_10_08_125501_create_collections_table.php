b8ff44d849beb1c0093bbb2752b75951b3c0e62a
TheBreasy <b.cautaerts@hotmail.be>
2021-10-08 15:53:13 +0200

    created tables and migrated them in database

---
+++ exspace/database/migrations/2021_10_08_125501_create_collections_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCollectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('collections', function (Blueprint $table) {
+            $table->id(); //is a big int not int
+            $table->timestamps();
+            $table->string("user_id");
+            $table->string("title");
+            $table->longText("description");
+            $table->string("image");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('collections');
+    }
+}
