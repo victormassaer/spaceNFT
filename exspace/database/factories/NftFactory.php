6e567c5eeae2aee742cdfe541c03dd90e201186e
TheBreasy <b.cautaerts@hotmail.be>
2021-10-08 18:14:43 +0200

    Added seeders (fake data) for Collection, Comment and Nft

---
+++ exspace/database/factories/NftFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Nft;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class NftFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Nft::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => $this->faker->numberBetween(1, 4),
+            'title' => $this->faker->company,
+            'description' => $this->faker->text(100),
+            'image' => $this->faker->imageUrl(500,500),
+            'is_minted' => $this->faker->boolean
+        ];
+    }
+}
