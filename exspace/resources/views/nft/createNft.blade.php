17678cf523347f2b4ec9d8cfa4db00927fc205ee
TheBreasy <b.cautaerts@hotmail.be>
2021-10-23 14:17:19 +0200

    created new layout for login and register without nav and added linking from register to login and vice versa

--- exspace/resources/views/nft/createNft.blade.php
+++ exspace/resources/views/nft/createNft.blade.php
@@ -6,9 +6,7 @@
 
     <div class="w-screen h-screen flex justify-center items-center">
 		<form class="p-10 bg-white rounded flex justify-center items-center flex-col shadow-md" method="post" action="{{url('/addNFT')}}">
-
         @csrf
-
 			<p class="mb-5 text-3xl uppercase text-gray-600">create NFT</p>
 			<input type="text" name="title" class="mb-5 p-3 w-80 focus:border-purple-700 rounded border-2 outline-none"  placeholder="title" required>
 			<input type="text" name="description" class="mb-5 p-3 w-80 focus:border-purple-700 rounded border-2 outline-none"  placeholder="description" required>
